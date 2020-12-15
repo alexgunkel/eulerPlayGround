842af22c2805f8baa48a98bc1486442e0e886325
Alexander Gunkel <alexander.gunkel@check24.de>
2020-12-15 20:19:36 +0100

    special treatment for number 1 and 2

--- src/735/main.cpp
+++ src/735/main.cpp
@@ -8,12 +8,13 @@ int main(int argc, char** argv) {
     eu_735::Integer i{100};
 
     uint64_t count{0};
+    std::map<uint64_t, uint64_t> real{};
     for (int j = 1; j <= i; ++j) {
         std::cout << j << ": ";
         for (int k = 1; k <= j; ++k) {
             if ((2*j*j) % k == 0) {
                 std::cout << k << ", ";
-                count++;
+                real[k]++;
             }
         }
         std::cout << "\n";
@@ -24,15 +25,23 @@ int main(int argc, char** argv) {
 
     Generator generator{i};
     uint64_t sum = 0;
+    std::map<uint64_t, uint64_t> counted{};
+    counted[1] = i;
+    counted[2] = i-1;
     for (const auto prime : generator.generate()) {
+        if (prime < 3) {
+            continue;
+        }
 
         for (uint64_t cur = prime, p=1; cur <= i; cur = power<uint64_t>(prime, ++p)) {
             uint64_t add = (i - cur)/power<uint64_t>(prime, (p+1)/2) + 1;
-            std::cout << prime << "  " << cur << ": " << add << "\n";
-            sum += add;
+            counted[cur] = add;
         }
     }
-    std::cout << sum << "\n";
+
+    for (uint64_t j = 1; j <= i; ++j) {
+        std::cout << j << " real: " << real[j] << ", counted: " << counted[j] << "\n";
+    }
 
     return 0;
 }
